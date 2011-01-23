b3dcf7ebac45e8800ed78db105ce31e03c43e49d
Borodin Vadim <vbo@rentsoft.ru>
2011-01-23 21:06:24 +0300

    example with english dic

--- test.php
+++ test.php
@@ -4,11 +4,17 @@ require_once 'lib/config.php';
 require_once 'PHP/I18n.php';
 require_once 'PHP/I18n/Backend/IniFile.php';
 
-$dic = new PHP_I18n(new PHP_I18n_Backend_IniFile('russian', dirname(__FILE__) . '/misc'));
+$dicRu = new PHP_I18n(new PHP_I18n_Backend_IniFile('russian', dirname(__FILE__) . '/misc'));
+$dicEng = new PHP_I18n(new PHP_I18n_Backend_IniFile('english', dirname(__FILE__) . '/misc'));
 
-echo $dic->get('hello');
-echo @$dic->get('hello1');
-echo $dic->get('dectest', array(
+echo $dicRu->get('hello');
+echo @$dicRu->get('hello1');
+echo $dicRu->get('dectest', array(
+    'item' => array(1, 'banan'),
+    'sum' => array(30, 'rub')
+));
+
+echo $dicEng->get('dectest', array(
     'item' => array(1, 'banan'),
     'sum' => array(30, 'rub')
 ));
