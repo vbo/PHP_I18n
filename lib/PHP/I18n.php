117cef1da0b37b44e324397db19aac38c8eabb09
Borodin Vadim <vbo@rentsoft.ru>
2011-01-23 19:26:53 +0300

    simple placeholders and functions-processors support added

--- lib/PHP/I18n.php
+++ lib/PHP/I18n.php
@@ -3,15 +3,59 @@
 class PHP_I18n
 {
     private static $_backend;
+    private static $_args;
 
     public static function init(PHP_I18n_Backend $backend)
     {
         self::$_backend = $backend;
     }
 
-    public static function get($literalId)
+    public static function get($literalId, $args=null)
     {
-        return self::$_backend->get($literalId);
+        $origin = self::$_backend->get($literalId);
+        return self::_expandPlaceholders($origin, $args);
+    }
+
+    private static function _expandPlaceholders($str, $args)
+    {
+        $result = $str;
+        self::$_args = $args;
+        if ($args) {
+            $result = preg_replace_callback('/\%([a-zA-Z]*)(\[[a-zA-Z0-9]+?\])+/',
+                array(__CLASS__, '_expandPlaceholder'), $result);
+        }
+        return $result;
+    }
+
+    private static function _expandPlaceholder($ms)
+    {
+        $fname = $ms[1] ? $ms[1] : 'print';
+        $paramPath = explode('][',
+            substr(self::_cutProcessorName($ms[0]), 1, -1)
+        );
+        
+        $curParam = self::$_args;
+        foreach ($paramPath as $pathKey) {
+            $curParam = $curParam[$pathKey];
+        }
+        return call_user_func_array(
+            array(__CLASS__, '_process' . ucfirst($fname)),
+            array($curParam));
+    }
+
+    private static function _processPrint($param)
+    {
+        return $param;
+    }
+
+    private static function _processDecl($param)
+    {
+        return 'TODO';
+    }
+
+    private static function _cutProcessorName($str)
+    {
+        return preg_replace('/%([a-zA-Z]*)/', '', $str);
     }
 
     private function __construct() {}
--- test.php
+++ test.php
@@ -7,3 +7,7 @@ require_once 'PHP/I18n/Backend/IniFile.php';
 PHP_I18n::init(new PHP_I18n_Backend_IniFile('ru', dirname(__FILE__) . '/misc'));
 
 echo @PHP_I18n::get('hello1');
+echo PHP_I18n::get('dectest', array(
+    'item' => array(5, 'banan'),
+    'sum' => array(30, 'rub')
+));
