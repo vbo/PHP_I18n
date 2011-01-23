ec5fce13a0e67bc562e72b08cbc50b79067b2f14
Borodin Vadim <vbo@rentsoft.ru>
2011-01-23 20:38:24 +0300

    test for text declension support

--- test.php
+++ test.php
@@ -4,10 +4,10 @@ require_once 'lib/config.php';
 require_once 'PHP/I18n.php';
 require_once 'PHP/I18n/Backend/IniFile.php';
 
-PHP_I18n::init(new PHP_I18n_Backend_IniFile('ru', dirname(__FILE__) . '/misc'));
+PHP_I18n::init(new PHP_I18n_Backend_IniFile('russian', dirname(__FILE__) . '/misc'));
 
 echo @PHP_I18n::get('hello1');
 echo PHP_I18n::get('dectest', array(
-    'item' => array(5, 'banan'),
+    'item' => array(1, 'banan'),
     'sum' => array(30, 'rub')
 ));
